3ed9cb2366ca21d703ae3b0e2dcc9bbecba13954
Pascal Cornu <pascal.cornu@gmx.ch>
2025-11-07 16:41:11 +0100

    conveyers finally consistent

--- Scripts/ConveyorBelt.gd
+++ Scripts/ConveyorBelt.gd
@@ -1,9 +1,20 @@
 extends MeshInstance3D
 
 # first number is m/s
-const SPEED = 2.0 / 1.5
+const SPEED = 2.0 / 3.0
+var material_instance: ORMMaterial3D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	var original_mat = get_surface_override_material(0)
+	if original_mat:
+		material_instance = original_mat.duplicate()
+		set_surface_override_material(0, material_instance)
+		
+	if material_instance and mesh:
+		var aabb = mesh.get_aabb()
+		var length = aabb.size.x * global_transform.basis.get_scale().x
+		material_instance.uv1_scale.x = length
+		
 func _process(delta: float) -> void:
-	get_surface_override_material(0).uv1_offset.x += delta * SPEED
+	if material_instance:
+		material_instance.uv1_offset.x += delta * SPEED
